310731b43a22bcc50edcd439ae347a4e2a4c5952
Mare <sevilamare@gmail.com>
2020-08-19 16:35:46 -0300

    json v0

--- src/index.js
+++ src/index.js
@@ -1,43 +1,8 @@
-import { renderTasks, renderProjects } from './renderTasks';
+import { renderProjects } from './renderTasks';
 
 // debug options - will reset the arrray on reload!
 localStorage.setItem('options', ['all', 'Todo', 'In progress', 'Done']);
-
-// GET NEW TASKS
-document.body.querySelector('#submit').addEventListener('click', (event) => {
-  const title = document.querySelector('#title').value;
-  const description = document.querySelector('#description').value;
-  const dueDate = document.querySelector('#dueDate').value;
-
-  const priority = document.querySelector('#priority');
-  const prioritySelected = priority.options[ priority.selectedIndex].text;
-
-  const project = document.querySelector('#project');
-  const projectSelected = project.options[project.selectedIndex].text;
-
-  // NEED CREATE TASK ARRAY KEY, WITH ALL TASKS INSIDE
-  const taskKey = title.toLowerCase().replace(/\s/g, '') + projectSelected.toLowerCase().replace(/\s/g, '');
-  const existKey = localStorage.getItem(taskKey);
-  if (existKey === null) {
-    localStorage.setItem(taskKey, [title, description, dueDate, prioritySelected, projectSelected]);
-    const existProject = localStorage.getItem(projectSelected);
-    // Create a key for each project, if it is already there,
-    // retrieve the tasks keys and add it the new task key. 
-    // Then replace and save again the new project keys. 
-    if(existProject === null) {
-      localStorage.setItem(projectSelected, [taskKey]);
-    } else {
-      const projectKeys = localStorage.getItem(projectSelected).split(',');
-      projectKeys.push(taskKey);
-      localStorage.setItem(projectSelected, projectKeys);
-    }
-  } else {
-    document.querySelector('#error').innerHTML = 'name already taken for this project.';
-  }
-
-  // renderProjects();
-  event.preventDefault();
-});
+localStorage.setItem('all', 'stuff');
 
 const fillOptions = () => {
   const all = localStorage.getItem('options').split(',');
@@ -48,7 +13,6 @@ const fillOptions = () => {
     elementsToAppend += `<option>${element}</option>`;
   }
   document.querySelector('#project').innerHTML += elementsToAppend;
-  // renderTasks();
 };
 
 const addNewOption = (cap) => {
@@ -57,8 +21,61 @@ const addNewOption = (cap) => {
   document.querySelector('#project').append(x);
 };
 
+document.body.onload = fillOptions;
 
-// get new project as option from form
+// GET NEW TASKS
+document.body.querySelector('#submit').addEventListener('click', (event) => {
+  // get data from form
+  const title = document.querySelector('#title').value;
+  const description = document.querySelector('#description').value;
+  const dueDate = document.querySelector('#dueDate').value;
+
+  const priority = document.querySelector('#priority');
+  const prioritySelected = priority.options[priority.selectedIndex].text;
+
+  const project = document.querySelector('#project');
+  const projectSelected = project.options[project.selectedIndex].text;
+
+
+
+  const arr = {title, description, dueDate, prioritySelected, projectSelected}
+
+  const OBJECT_WITH_ALL_DATA = {
+    todo: arr
+  }
+
+  const projectsKeys = localStorage.getItem('')
+
+
+  localStorage.setItem('PROJECTS', JSON.stringify(OBJECT_WITH_ALL_DATA) )
+  const retrieve = localStorage.getItem('PROJECTS')
+
+  const unpack = JSON.parse(retrieve)
+  console.log(unpack['todo']['projectSelected'])
+  // console.log(localStorage)
+
+
+  // const taskKey = title.toLowerCase().replace(/\s/g, '') + projectSelected.toLowerCase().replace(/\s/g, '');
+  // const existKey = localStorage.getItem(taskKey);
+  // if (existKey === null) {
+  //   localStorage.setItem(taskKey, [title, description, dueDate, prioritySelected, projectSelected]);
+  //   const existProject = localStorage.getItem(projectSelected);
+
+  //   if (existProject === null) {
+  //     localStorage.setItem(projectSelected, [taskKey]);
+  //   } else {
+  //     const projectKeys = localStorage.getItem(projectSelected).split(',');
+  //     projectKeys.push(taskKey);
+  //     localStorage.setItem(projectSelected, projectKeys);
+  //   }
+  // } else {
+  //   document.querySelector('#error').innerHTML = 'name already taken for this project.';
+  // }
+
+  // renderProjects();
+  event.preventDefault();
+});
+
 document.querySelector('#newProjBtn')
   .addEventListener('click', (event) => {
     // 1 get value form input
@@ -77,7 +94,4 @@ document.querySelector('#newProjBtn')
     event.preventDefault();
   });
 
-document.body.onload = fillOptions;
-
-renderProjects();
-// renderTasks();
+// renderProjects();
--- src/renderTasks.js
+++ src/renderTasks.js
@@ -1,47 +1,69 @@
-import { createContent, createContainer } from "./helpers";
+import { createContent, createContainer } from './helpers';
 
-const renderProjects = () => {
-  const projects = localStorage.getItem("options").split(",");
-  if (projects) {
-    for (let project = 0; project < projects.length; project += 1) {
-      const element = projects[project];
-      const containerId = element.replace(/\s/g, "");
-      const container = createContainer("div", null, containerId);
-      const title = createContent("h2", null, element);
-      container.append(title);
-      document.querySelector("#tasks").append(container);
-      renderTasks(element, containerId);
-    }
-  }
-};
+// input example (In progress, inprogress)
+// projectKey is key to 
+// (In progress) is key with tasks keys
+
+
+// 1 ALL VALIDIS PROJECTS LIST
+// [PROJ1, PROJ2]
+
+// 2 GET ALL TASKS FROM A SINGLE PROJECT
+// PROJ1: [TASKS1, TAKSK2]
 
 const renderTasks = (projectKey, projectId) => {
-  //data
-  if (projectKey === null)
-    return;
+  if (localStorage.getItem(projectKey) === null) { return; }
+  console.log('projectkey', projectKey)
+  console.log('projectId', projectId)
 
-  console.log(projectKey);
-  const projectTasksKeys = localStorage.getItem(projectKey).split(","); // check when local storage doesn't have a key with the project name.
+  // check when local storage doesn't have a key with the project name.
+  const projectTasksKeys = localStorage.getItem(projectKey).split(',');
 
   if (projectTasksKeys) {
     for (let taskKey = 0; taskKey < projectTasksKeys.length; taskKey += 1) {
       const element = projectTasksKeys[taskKey];
-      const task = localStorage.getItem(element).split(",");
+      const task = localStorage.getItem(element).split(',');
       if (task) {
         const title = task[0];
         const description = task[1];
         const date = task[2];
         const priority = task[3];
+
         // Containers
         const taskDiv = document.querySelector(`#${projectId}`);
-        const projDiv = createContainer("div", "proj-div");
+        const projDiv = createContainer('div', 'proj-div');
+
         // Content
-        const projTitle = createContent("h3", null, title);
-        const projDescription = createContent("p", null, description);
-        const projDate = createContent("p", null, date);
-        const projPriority = createContent("p", null, priority);
+        const projTitle = createContent('h3', null, title);
+        const projDescription = createContent('p', null, description);
+        const projDate = createContent('p', null, date);
+        const projPriority = createContent('p', null, priority);
+
         projDiv.append(projTitle, projDescription, projDate, projPriority);
         taskDiv.append(projDiv);
+      }
+    }
+  }
+};
+
+const renderProjects = () => {
+  const projects = localStorage.getItem('options').split(',');
+
+  if (projects) {
+    for (let project = 0; project < projects.length; project += 1) {
+  
+      const element = projects[project];
+      const containerId = element.replace(/\s/g, '');
+  
+      const container = createContainer('div', null, containerId);
+      const title = createContent('h2', null, element);
+  
+      container.append(title);
+      document.querySelector('#tasks').append(container);
+
+      if (localStorage.getItem(element)) {
+        console.log(element) // each project key
+        renderTasks(element, containerId);
       }
     }
   }
